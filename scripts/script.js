c76b5688000c5dce24e85ea297e52ba4f7181cfd
Jhon Philip Villegas <jhonphilipvillegas7@gmail.com>
2026-05-09 09:49:17 +0800

    Add more pics and google font

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+anime({
+    targets: '.hero, .tech-card, .cert-card',
+    translateY: [50, 0],
+    opacity: [0, 1],
+    duration: 1500,
+    easing: 'easeOutExpo',
+    delay: anime.stagger(200)
+});
