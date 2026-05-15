1745ef2e6be785279032f0d7a3e0fd35831a60be
LordGrim13850 <jhonphilipvillegas7@gmail.com>
2026-05-15 15:18:35 +0800

    Fixed according to suggestions

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,32 @@ function closeModal() {
 
     modal.classList.remove("show");
 }
+
+
+const text = "Aspiring Front-End Developer";
+const typingElement = document.getElementById("typing");
+
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        typingElement.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 100);
+    }
+}
+
+typeEffect();
+
+window.addEventListener("scroll", () => {
+    const cards = document.querySelectorAll(".tech-card, .cert-card");
+
+    cards.forEach(card => {
+        const cardTop = card.getBoundingClientRect().top;
+
+        if (cardTop < window.innerHeight - 100) {
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }
+    });
+});
